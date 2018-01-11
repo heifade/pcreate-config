457bc2395ef07156a030118694982f0cdccebab7
heifade <heifade@126.com>
2018-01-11 16:00:30 +0800

    1.0.12

---
+++ project-config.ts
@@ -0,0 +1,18 @@
+import { ProjectConfigModel } from "./src/ProjectConfigModel";
+
+let projectConfig: ProjectConfigModel = {
+  projectType: "node",
+  compile: {
+    outDir: "./es/",
+    module: "commonjs",
+    target: "es5",
+    lib: ["es2015", "es2015.promise", "es2015.symbol"],
+    declaration: true
+  },
+  command: false,
+  documents: true,
+  unitTest: true,
+  sourceInclude: ["./src/**/*"]
+};
+
+export default projectConfig;
